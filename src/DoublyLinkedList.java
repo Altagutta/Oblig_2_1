55a5a638b02100361c1dd5f29ca1dad91f1a2ffa
pessflaska <severinlyngstad@live.no>
2018-10-10 12:18:09 +0200

    Adding datafields first, last, size. Implemeting methods: - getFirst - getLast - addFirst - addLast - add

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -4,31 +4,80 @@ import java.util.Iterator;
 // Alle metoder som tar inn en index skal kaste indexoutofbound exception
 //
 public class DoublyLinkedList<E> implements MyList<E> {
-    // Datamedlemmer
+    //  <SEVERIN>
+    private Node<E> first, last;
+    private int size = 0;
+    //  </SEVERIN>
 
-    /** Create a default list */
     public DoublyLinkedList() {}
 
     /** Create a list from an array of objects */
     public DoublyLinkedList(E[] objects) {}
 
+    //  <SEVERIN>
     /** Return the head element in the list */
-    public E getFirst() {}
+    public E getFirst() {
+        if(size == 0) {
+            return null;
+        }
+        return first.element;
+    }
 
     /** Return the last element in the list */
-    public E getLast() {}
+    public E getLast() {
+        if(size == 0) {
+            return null;
+        }
+        return last.element;
+    }
 
     /** Add an element to the beginning of the list */
-    public void addFirst(E e) {}
+    public void addFirst(E e) {
+        Node<E> newNode = new Node<>(e);
+        newNode.next = first;
+        first.previous = newNode;
+        first = newNode;
+        size++;
+
+        if (last == null) { last = first; }
+    }
 
     /** Add an element to the end of the list */
-    public void addLast(E e) {}
+    public void addLast(E e) {
+        Node<E> newNode = new Node<>(e);
+        if (last == null) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+            newNode.previous = last;
+            last = newNode;
+        }
+        size++;
+    }
 
     /**
      * Add a new element at the specified index in this list The index of the
      * head element is 0
      */
-    public void add(int index, E e) {}
+    public void add(int index, E e) {
+        if (index == 0) {
+            addFirst(e);
+        } else if (index >= size) {
+            addLast(e);
+        } else {
+            Node<E> current = first;
+            for (int i = 1; i < index; i++) {
+                current = current.next;
+            }
+            Node<E> temp = current.next;
+            current.next = new Node<>(e);
+            (current.next).next = temp;
+            temp.previous = current.next;
+            (current.next).previous = current;
+            size++;
+        }
+    }
+    //  </SEVERIN>
 
     /**
      * Remove the head node and return the object that is contained in the
