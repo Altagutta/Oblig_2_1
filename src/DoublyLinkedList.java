0ba1743c9ff21c8ffbb887b1f9e6059cb60f3a84
Sh1nku <hjandreas@hotmail.no>
2018-10-10 12:36:36 +0200

    Removing linkedListIterator and putting it back into DoublyLinkedList

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -90,4 +90,30 @@ public class DoublyLinkedList<E> implements MyList<E> {
     public Iterator<E> iterator() {
         return null;
     }
+
+    private class LinkedListIterator implements java.util.ListIterator<E> {
+        // datamedlemmer
+        public LinkedListIterator() {}
+
+
+        public LinkedListIterator(int index) {}
+
+        public void setLast() {}
+
+        @Override
+        public boolean hasNext() {}
+
+        @Override
+        public E next() {}
+
+
+        @Override
+        public boolean hasPrevious() {}
+
+        @Override
+        public E previous() {}
+
+        @Override
+        public void set(E e) {}
+    }
 }
